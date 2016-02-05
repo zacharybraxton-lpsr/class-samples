e9c9975c4d6a29982f6e90840815a69394e4cfe4
zacharybraxton-lpsr <rc.braxton.zachary@leadps.org>
2016-02-05 23:35:37 +0000

    A really cool poem program

---
+++ 4-2WritingFiles/haikuGenerator.py
@@ -0,0 +1,23 @@
+# make a welcom statment
+print("Welcome to the Haiku Generator! Lets make a haiku!")
+# make  a list that stores the stanzas
+poemList = []
+# make a questionare that asks the user for each line and appends it to poemList
+print("Provide the first line:")
+line1  = raw_input()
+poemList.append(line1)
+print("Provide the second line:")
+line2 = raw_input()
+poemList.append(line2)
+print("Provide the third line:")
+line3 = raw_input()
+poemList.append(line3)
+# ask the user what they would like
+print("What would you like to call the file you put your haiku in to?")
+haikuPoem = raw_input()
+file = open(haikuPoem , 'a')
+# make a loop that appends each line to the bottem of the file
+for list in poemList:
+	file.write(list + "\n")
+# make a congratulations message that tells the user how to view their poem
+print("Congradulation! To see your haiku just type cat with a space along with your file name!")
