893ada7f5b2d68eb1df8f1d545125ed21107f4d9
zacharybraxton-lpsr <rc.braxton.zachary@leadps.org>
2016-02-09 12:46:23 -0800

    Update haikuGenerator.py

--- 4-2WritingFiles/haikuGenerator.py
+++ 4-2WritingFiles/haikuGenerator.py
@@ -13,11 +13,11 @@ print("Provide the third line:")
 line3 = raw_input()
 poemList.append(line3)
 # ask the user what they would like
-print("What would you like to call the file you put your haiku in to?")
+print("What would you like to call the file you put your haiku in to? *REMEMBER: every file has to have no spaces and must be followed by .txt*")
 haikuPoem = raw_input()
 file = open(haikuPoem , 'a')
 # make a loop that appends each line to the bottem of the file
 for list in poemList:
 	file.write(list + "\n")
 # make a congratulations message that tells the user how to view their poem
-print("Congradulation! To see your haiku just type cat with a space along with your file name!")
+print("Congratulation! To see your haiku just type cat with a space along with your file name!")
